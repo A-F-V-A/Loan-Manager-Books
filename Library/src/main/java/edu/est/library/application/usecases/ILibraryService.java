77bf04999d84dccd7d4726ab8673f7059be940e7
AFVA <andresf.villarragaa@gmail.com>
2023-09-30 22:22:20 -0500

    Se avanza con la logica de los detalles

--- Library/src/main/java/edu/est/library/application/usecases/ILibraryService.java
+++
@@ -1,8 +0,0 @@
-package edu.est.library.application.usecases;
-
-import edu.est.library.domain.interfaces.ICreate;
-import edu.est.library.domain.interfaces.ITolist;
-
-public interface ILibraryService <Entity> extends ITolist<Entity>, ICreate<Entity> {
-
-}
